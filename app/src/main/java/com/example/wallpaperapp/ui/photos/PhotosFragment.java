e46272562925bde41e2f17dfc3295ea0eb570fdd
Rafi6677 <rafalgiemza6677@gmail.com>
2020-03-07 19:39:36 +0100

    Project prepared

---
+++ app/src/main/java/com/example/wallpaperapp/ui/photos/PhotosFragment.java
@@ -0,0 +1,39 @@
+package com.example.wallpaperapp.ui.photos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.wallpaperapp.R;
+
+public class PhotosFragment extends Fragment {
+
+    private PhotosViewModel photosViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        photosViewModel = ViewModelProviders.of(this).get(PhotosViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        final TextView textView = root.findViewById(R.id.text_collections);
+
+        photosViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/wallpaperapp/ui/photos/PhotosViewModel.java
@@ -0,0 +1,19 @@
+package com.example.wallpaperapp.ui.photos;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PhotosViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public PhotosViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is photos fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
