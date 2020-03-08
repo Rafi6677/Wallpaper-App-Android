af0c387261418dc9cad932b4fe0c8285b70d7cd3
Rafi6677 <rafalgiemza6677@gmail.com>
2020-03-08 16:48:35 +0100

    Getting photos from Unsplash

--- app/src/main/java/com/example/wallpaperapp/fragments/PhotosFragment.java
+++ app/src/main/java/com/example/wallpaperapp/fragments/PhotosFragment.java
@@ -1,9 +1,11 @@
 package com.example.wallpaperapp.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,30 +13,102 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wallpaperapp.R;
+import com.example.wallpaperapp.adapters.PhotosAdapter;
+import com.example.wallpaperapp.models.Photo;
 import com.example.wallpaperapp.viewmodels.PhotosViewModel;
+import com.example.wallpaperapp.webservices.ApiInterface;
+import com.example.wallpaperapp.webservices.ServiceGenerator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PhotosFragment extends Fragment {
 
+    private final String TAG = PhotosFragment.class.getSimpleName();
+
+    @BindView(R.id.fragment_photos_progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.fragment_photos_recyclerView)
+    RecyclerView recyclerView;
+
+    private Unbinder unbinder;
     private PhotosViewModel photosViewModel;
+    private PhotosAdapter photosAdapter;
+    private List<Photo> photos = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        photosViewModel = ViewModelProviders.of(this).get(PhotosViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_photos, container, false);
+        unbinder = ButterKnife.bind(this, view);
 
-        View root = inflater.inflate(R.layout.fragment_photos, container, false);
+        photosAdapter = new PhotosAdapter(getActivity(), photos);
 
-        //final TextView textView = root.findViewById(R.id.text_collections);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(photosAdapter);
 
-        photosViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        showProgressBar(true);
+        getPhotos();
+
+        return view;
+    }
+
+    private void getPhotos() {
+
+        ApiInterface apiInterface = ServiceGenerator.createService(ApiInterface.class);
+        Call<List<Photo>> call = apiInterface.getPhotos();
+
+        call.enqueue(new Callback<List<Photo>>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                //textView.setText(s);
+            public void onResponse(@NotNull Call<List<Photo>> call, @NotNull Response<List<Photo>> response) {
+
+                if (response.isSuccessful()) {
+                    photos.addAll(response.body());
+                    photosAdapter.notifyDataSetChanged();
+                } else {
+                    Log.e(TAG, "fail " + response.message());
+                }
+
+                showProgressBar(false);
+            }
+
+            @Override
+            public void onFailure(Call<List<Photo>> call, Throwable t) {
+
+                Log.e(TAG, "fail " + t.getMessage());
+                showProgressBar(false);
             }
         });
+    }
 
-        return root;
+    private void showProgressBar(boolean isShow) {
+
+        if (isShow) {
+            progressBar.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
